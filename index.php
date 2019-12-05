8620b6c306f6fcb9320fac687e183c7c9378d344
Scott <flyfamilyguy@gmail.com>
2019-12-05 14:44:30 -0800

    Create index.php
    index.php :  accepts all information accessable to user.

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * index.php
+ * This script gets all the data displayed
+ * to websites visitors.
+ * In reference to MVC, its the only page
+ * directly accessed by a user.
+ */
+
+// Call the applications initializer
+require 'Core\Class.application.php';
+// execute method statically
+Application::init();
