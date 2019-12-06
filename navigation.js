7c0475ead6240d37254313825d1af00053b2613e
Scott <flyfamilyguy@gmail.com>
2019-12-06 10:26:45 -0800

    Create navigation.js
    Drives our main navigation links within website pages

---
+++ navigation.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	
+	$("#open").click(function() {
+		
+		$(".navigation").slideDown();
+	});
+    $("#close").click(function() {
+		
+		$(".navigation").slideUp();
+	});
+});
